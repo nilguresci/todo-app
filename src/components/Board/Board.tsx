c0c8795bf635b06de3f2e612f43caa37e89f2a3e
Nil Güreşçi <54067949+nilguresci@users.noreply.github.com>
2024-01-15 17:10:39 +0300

    Sortable

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useDroppable } from "@dnd-kit/core";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import Task from "../Task/Task";
+import { useState } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Board = (props: any) => {
@@ -12,9 +29,43 @@ const Board = (props: any) => {
     padding: "50px",
   };
 
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items: any) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
     <div ref={setNodeRef} style={style}>
-      {props.children}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {items.map((id: any) => (
+            <Task key={id} id={id}>
+              {" "}
+              Selamlar Ben {id}{" "}
+            </Task>
+          ))}
+        </SortableContext>
+      </DndContext>
     </div>
   );
 };
